e24a1b64d0627b29f20c8fd16aa179b61722555a
Suguru Tokuda <Suguru@sugurus-mac-mini.local>
2018-11-18 20:59:01 -0600

    Initial Commit

---
+++ Executor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Executor
+//
+//  Created by Suguru on 11/18/18.
+//  Copyright © 2018 Executor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Executor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Executor
+//
+//  Created by Suguru on 11/18/18.
+//  Copyright © 2018 Executor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
